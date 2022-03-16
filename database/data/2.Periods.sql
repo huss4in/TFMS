854d7e9755c2d6fe063ee5332949c3acf2b8f8fe
Hussain <huss4in7@outlook.com>
2022-03-17 00:00:00 +0300

    🛢 Data Layer

---
+++ database/data/2.Periods.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+  tfms.periods (
+    period_name,
+    program_id,
+    fund_estimated_status,
+    fund_estimation_date
+  )
+VALUES
+  ("2021Q1", 1, 'not started', 0),
+  ("2021Q2", 1, 'not started', 0),
+  ("2022Q1", 1, 'not started', 0),
+  ("2021Q2", 2, 'not started', 0),
+  ("2022Q1", 2, 'not started', 0);
---
+++ database/data/4.Features-Catagories.sql
@@ -0,0 +1,22 @@
+INSERT INTO
+  tfms.feature_catagories (id, feature_id, name)
+VALUES
+  (1, 1, "Sales and trading"),
+  (2, 1, "Food and Beverage"),
+  (3, 1, "Research and development"),
+  (4, 1, "Support services"),
+  (5, 1, "Building, Construction & Real Estate"),
+  (6, 1, "IT"),
+  (7, 1, "Manufacturing"),
+  (8, 1, "Medical"),
+  (9, 1, "Repair and maintenance"),
+  (10, 1, "Education"),
+  (11, 1, "Entertainment"),
+  (12, 1, "Fintech"),
+  (13, 1, "Storage"),
+  (14, 1, "Agriculture"),
+  (15, 1, "Medical"),
+  (16, 1, "OTHER"),
+  (17, 4, "Start-up"),
+  (18, 4, "Small-Medium Enterprise"),
+  (19, 4, "Seed stage");
